7d2c5a69a5a74d279d6f68c0f2f63ea8233f8ffa
Homxy <120322865+Homxy@users.noreply.github.com>
2022-12-13 16:46:57 +0700

    Add files via upload

---
+++ lab7_1.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+string func1(string x){
+	int i = 0, L = x.size();
+	string y = "";
+	while(i < L){
+		y += x[L-i-1];
+		i++;
+	}
+	return y;
+}
+
+string func2(string x){
+	int i = 0, L = x.size();
+	string y = "";
+	while(i < L){
+		y += toupper(x[i]);
+		i++;
+	}
+	return y;	
+}
+
+string func3(string x){
+	int i = 0, L = x.size();
+	string y = "";
+	while(i < L){
+		y += tolower(x[i]);
+		i++;
+	}
+	return y;	
+}
+
+int main(){
+    string text ="";
+    string status = "";
+    cout << "Input text: ";
+    cin>>text;
+    cout << "Reversed text: "<< func1(text) <<endl;
+    if(func2(text)==func2(func1(text))){
+        status = "Yes";
+    }
+    else{
+        status = "No";
+    }
+    cout << "Palindrome: " << status;
+
+    return 0;
+}
